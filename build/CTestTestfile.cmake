ad05785132f24ca76e82515d39dbd3231586e1f2
FedericoSolari <fsolariv@fi.uba.ar>
2023-10-25 20:55:43 -0300

    tests

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/fede/Documentos/Taller/tp-worms
+# Build directory: /home/fede/Documentos/Taller/tp-worms/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(Test1 "/home/fede/Documentos/Taller/tp-worms/build/test1")
+set_tests_properties(Test1 PROPERTIES  _BACKTRACE_TRIPLES "/home/fede/Documentos/Taller/tp-worms/CMakeLists.txt;81;add_test;/home/fede/Documentos/Taller/tp-worms/CMakeLists.txt;0;")
